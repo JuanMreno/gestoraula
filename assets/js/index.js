bc30adff2e4a4e3413dcde9c7dec4aa394f8c43f
juanmoreno1106 <jucamoreno@utp.edu.co>
2016-10-26 10:07:15 -0500

    Initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,27 @@
+
+(function($) {
+
+	skel.breakpoints({
+		wide: '(min-width: 961px) and (max-width: 1880px)',
+		normal: '(min-width: 961px) and (max-width: 1620px)',
+		narrow: '(min-width: 961px) and (max-width: 1320px)',
+		narrower: '(max-width: 960px)',
+		mobile: '(max-width: 736px)'
+	});
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body');
+
+		// Disable animations/transitions until the page has loaded.
+		$body.addClass('is-loading');
+
+		$window.on('load', function() {
+			$body.removeClass('is-loading');
+
+			$('#cont').load('views/login.html');
+			//$('#cont').load('views/main.html',mainInit);
+		});
+	});
+})(jQuery);
---
+++ assets/js/jsGrid/db.js
@@ -0,0 +1,44 @@
+(function() {
+
+    var db = {
+
+        loadData: function(filter) {
+            return $.grep(this.clients, function(client) {
+                return (!filter.Practica || client.Practica.indexOf(filter.Practica) > -1)
+                    && (!filter.Unidad || client.Unidad.indexOf(filter.Unidad) > -1)
+                    && (!filter.Fecha || client.Fecha.indexOf(filter.Fecha) > -1)
+                    && (filter.Estado === undefined || client.Estado === filter.Estado);
+            });
+        },
+
+        insertItem: function(insertingClient) {
+            this.clients.push(insertingClient);
+        },
+
+        updateItem: function(updatingClient) { },
+
+        deleteItem: function(deletingClient) {
+            var clientIndex = $.inArray(deletingClient, this.clients);
+            this.clients.splice(clientIndex, 1);
+        }
+
+    };
+
+    window.db = db;
+
+    db.clients = [
+        { "Id":1, "Practica": "Ondas estacionárias", "Unidad": "Mecánica clásica", "Fecha": "25/10/2016", "Estado": true },
+        { "Id":1, "Practica": "Extensiometria y transductores", "Unidad": "Mecánica clásica", "Fecha": "25/10/2016", "Estado": false },
+        { "Id":1, "Practica" : "Péndulo balístico", "Unidad": "Mecánica clásica", "Fecha": "25/10/2016", "Estado": false },
+        { "Id":1, "Practica" : "Medida de la potencia de pedaleao", "Unidad": "Mecánica clásica", "Fecha": "25/10/2016", "Estado": true },
+        { "Id":1, "Practica" : "Movimieto bajo fuerzas centrales", "Unidad": "Mecánica clásica", "Fecha": "25/10/2016", "Estado": true },
+        { "Id":1, "Practica" : "Osciloscopio", "Unidad": "Electromagnetismo", "Fecha": "25/10/2016", "Estado": true },
+        { "Id":1, "Practica" : "Ciclo de histeresis", "Unidad": "Electromagnetismo", "Fecha": "25/10/2016", "Estado": false },
+        { "Id":1, "Practica" : "Campo magnetico terrestre", "Unidad": "Electromagnetismo", "Fecha": "25/10/2016", "Estado": true },
+        { "Id":1, "Practica" : "Magnetico-Conductor", "Unidad": "Electromagnetismo", "Fecha": "25/10/2016", "Estado": false },
+        { "Id":1, "Practica" : "Acciones entre corrientes", "Unidad": "Electromagnetismo", "Fecha": "25/10/2016", "Estado": true },
+        { "Id":1, "Practica" : "Campos magnéticos", "Unidad": "Electromagnetismo", "Fecha": "25/10/2016", "Estado": true }
+    ];
+
+
+}());
